da4adbb492c5213a2322391febd251385c1253b3
Sam <livaco@livaco.dev>
2025-12-10 21:42:51 +0000

    fix: hide camera_id and camera_instrument_name in the results

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/rovers', function (Request $request) {
-    return Rover::with("cameras")->get();
+    return Rover::with('cameras')->get();
 });
 Route::get('/rovers/{roverID}', function (Request $request, string $roverID) {
-    return Rover::with("cameras")->find($roverID);
+    return Rover::with('cameras')->find($roverID);
 });
 
 Route::get('/cameras', function (Request $request) {
@@ -24,13 +24,20 @@ Route::get('/cameras/{cameraID}', function (Request $request, string $cameraID) 
 Route::get('/images/{roverID}', function (GetRoverImagesRequest $request, string $roverID) {
     $validated = collect($request->validated());
 
-    $data = RoverImage::with("camera")->where('rover_id', $roverID);
+    $data = RoverImage::with('camera')->where('rover_id', $roverID);
     if($validated->has('sol'))
         $data = $data->where('sol', $validated->get('sol'));
     if($validated->has('camera')) {
-        $cameraID = $roverID . "-" . $validated->get('camera');
+        $cameraID = $roverID . '-' . $validated->get('camera');
         $data = $data->where('camera_id', $cameraID);
     }
 
-    return $data->paginate($validated->get("per_page", 15));
+    // https://stackoverflow.com/a/51131009
+    $result = $data->paginate($validated->get('per_page', 15));
+    $data = $result;
+    $result = $result->makeHidden('camera_id');
+    $result = $result->makeHidden('camera_instrument_name');
+    $data->data = $result;
+
+    return $data;
 });
